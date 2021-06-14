2876893ebaaec67b21e47fe5e522815ebde669dc
Ronald Mundell <ronniemundell8@gmail.com>
2021-06-14 14:28:10 -0700

    change?

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -60,7 +60,7 @@ public class Main {
       String sql = "INSERT INTO rectangles (name, height, width, color) VALUES ('" + rectangle.getName() + "','" + rectangle.getHeight() + "','" + rectangle.getWidth() + "','" + rectangle.getBgcolor() + "')";
       stmt.executeUpdate(sql);
       System.out.println(rectangle.getName());
-      return "redirect:/";
+      return "redirect:/newrectangle/rectangle";
     } catch (Exception e) {
       model.put("message", e.getMessage());
       return "error";
