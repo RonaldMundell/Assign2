aa9c208a2da96c673733eeef3af1d000b3ba7603
Ronald <Ronniemundell8@gmail.com>
2021-06-19 16:36:30 -0700

    javascript

---
+++ src/main/resources/templates/table.js
@@ -0,0 +1,10 @@
+function CreateTable(Table){
+    var tables = "";
+    console.log("Hello")
+    Table.forEach(rectangle => {
+       tables =+ "<tr><td>"+rectangle.id+"</td><td>"+rectangle.getName()
+       +"</td><td>"+rectangle.getColor()+'</td><td><button value ="Edit Button" href="th:'
+       +rectangle.getName()+'"></td></tr>';
+    })
+    document.getElementById("TheTable").append(tables)
+}
