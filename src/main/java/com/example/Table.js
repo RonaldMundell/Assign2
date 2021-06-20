cd55920d5bf0467b39833cc010a0c5366e1a8213
Ronald <Ronniemundell8@gmail.com>
2021-06-19 18:11:24 -0700

    heroku

--- src/main/java/com/example/Table.js
+++ src/main/java/com/example/Table.js
@@ -1,6 +1,11 @@
 function createTable(Table){
-    var tables = "";
+    var tables = "<tr><td>Enter Data</td></tr>";
+    var i = 0;
     /*Table.forEach(rectangle => {
+        if(i == 0 ){
+            tables = "";
+            i = 2;
+        };
        tables =+ "<tr><td>"+rectangle.id+"</td><td>"+rectangle.getName()
        +"</td><td>"+rectangle.getColor()+'</td><td><button value ="Edit Button" href="th:'
        +rectangle.getName()+'"></td></tr>';
