7756560770ff539b97e9699ab363aff43d379099
Ronald <Ronniemundell8@gmail.com>
2021-06-20 10:28:03 -0700

    work

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -82,7 +82,7 @@ public class Main {
     try (Connection connection = dataSource.getConnection()) {
       Statement stmt = connection.createStatement();
       if(model.isEmpty() == false){
-          newrectangle = model.get("newrectangle");
+          newrectangle.setName(model.get("newrectangle").getName());
       }
       stmt.executeUpdate("CREATE TABLE IF NOT EXISTS rectangles (id serial, name varchar(20), height varchar(20), width varchar(20), color varchar(20))");
       String sql = "INSERT INTO rectangles (name, height, width, color) VALUES ('" + newrectangle.getName() + "','" + newrectangle.getHeight() + "','" + newrectangle.getWidth() + "','" + newrectangle.getBgcolor() + "');";
