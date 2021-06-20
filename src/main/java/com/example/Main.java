dc7bb6054e9bec2f978e8b148edd65944a007edb
Ronald <Ronniemundell8@gmail.com>
2021-06-20 11:05:56 -0700

    lets

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -79,7 +79,9 @@ public class Main {
     try (Connection connection = dataSource.getConnection()) {
       Statement stmt = connection.createStatement();
       stmt.executeUpdate("CREATE TABLE IF NOT EXISTS rectangles (id serial, name varchar(20), height varchar(20), width varchar(20), color varchar(20))");
-      
+      if(model.containsKey("rectagle")){
+        rectangle.setHeight("20");
+      }
       String sql = "INSERT INTO rectangles (name, height, width, color) VALUES ('" + rectangle.getName() + "','" 
       + rectangle.getHeight() + "','" + rectangle.getWidth() + "','" + rectangle.getBgcolor() + "');";
       stmt.executeUpdate(sql);
