94553dd15ca2e27d9ba846745f4ee6c27f1586ae
Ronald <Ronniemundell8@gmail.com>
2021-06-19 18:09:01 -0700

    love

--- src/main/java/com/example/Table.js
+++ src/main/java/com/example/Table.js
@@ -1,10 +1,10 @@
 function createTable(Table){
     var tables = "";
-    console.log("Hello")
-    Table.forEach(rectangle => {
+    /*Table.forEach(rectangle => {
        tables =+ "<tr><td>"+rectangle.id+"</td><td>"+rectangle.getName()
        +"</td><td>"+rectangle.getColor()+'</td><td><button value ="Edit Button" href="th:'
        +rectangle.getName()+'"></td></tr>';
     })
+    */
     document.getElementById("TheTable").append(tables)
 }
