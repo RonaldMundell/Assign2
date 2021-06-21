ca0c1c147a85350a98fd942d2e55682f95a502bb
Ronald <Ronniemundell8@gmail.com>
2021-06-20 21:32:49 -0700

    yes

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -106,7 +106,7 @@ public class Main {
       rectangle.setBgcolor(rs.getString("color"));
       rectangle.setId(rs.getString("id"));
       model.put("rectangle", rectangle);
-      model.put("boxstyle", "padding: 20px border: 1px solid black; width: "+rectangle.getWidth()+"px; height: "+rectangle.getHeight()+"px; background-color: "+rectangle.getBgcolor()+";");
+      model.put("boxstyle", "margin: center; padding: 20px border: 1px solid black; width: "+rectangle.getWidth()+"px; height: "+rectangle.getHeight()+"px; background-color: "+rectangle.getBgcolor()+";");
       return "rectangle";
       }catch (Exception e){
         model.put("Message", e.getMessage());
