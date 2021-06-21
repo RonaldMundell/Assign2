f96aae723ef254f9d40f0463d9b0320ec46ba251
Ronald <Ronniemundell8@gmail.com>
2021-06-20 18:02:47 -0700

    yes

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -21,7 +21,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
-import java.util.CheckedEntry;
 import java.util.Map;
 
 @Controller
